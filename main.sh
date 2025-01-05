872f568e17577af2d53d26889461f8035c6f9e24
kecajtop <kecajtop@wp.pl>
2025-01-05 22:42:57 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -8,8 +8,10 @@
 # Category   :    Shell menu tools
 #######################################################################
 
-source ./backup.sh
-source ./update.sh
+DIR=/root
+
+source $DIR/backup.sh
+source $DIR/update.sh
 
 ip=192.168.1.1
 
@@ -17,7 +19,7 @@ function main_menu()
 {
 choise=$(whiptail --title "Operative Systems" --menu "Make your choice" 16 100 10\
 	"1)" "WiFi"   \
-	"2)" ""  \
+	"2)" "Webpage SSL cert fix"  \
 	"3)" "" \
 	"4)" "" \
 	"5)" "" \
@@ -34,7 +36,18 @@ echo $choise
 		wifi_menu
 	;;
 	"2)")
-		echo
+		opkg update && opkg install openssl-util luci-app-uhttpd
+		echo -e "[req]\ndistinguished_name  = req_distinguished_name \nx509_extensions     = v3_req \nprompt	= no \nstring_mask         = utf8only \n" > /etc/ssl/myconfig.conf
+		echo -e "[req_distinguished_name] \nC                   = US \nST                  = VA \nL                   = SomeCity" >> /etc/ssl/myconfig.conf
+		echo -e "O                   = OpenWrt \nOU                  = Home Router \nCN                  = luci.openwrt \n" >> /etc/ssl/myconfig.conf
+		echo -e "[v3_req] \nkeyUsage            = nonRepudiation, digitalSignature, keyEncipherment \nextendedKeyUsage    = serverAuth \nsubjectAltName      = @alt_names\n" >>/etc/ssl/myconfig.conf
+		echo -e "[alt_names]\nDNS.1               = luci.openwrt\nIP.1                = 192.168.2.1\n" >> /etc/ssl/myconfig.conf
+		cd /etc/ssl
+		openssl req -x509 -nodes -days 730 -newkey rsa:2048 -keyout mycert.key -out mycert.crt -config myconfig.conf
+		uci set uhttpd.main.cert='/etc/ssl/mycert.crt'
+		uci set uhttpd.main.key='/etc/ssl/mycert.key'
+		uci commit
+		/etc/init.d/uhttpd restart
 	;;
 	"3)")
 		echo
@@ -52,8 +65,7 @@ echo $choise
 		update_menu
 	;;	
 	"7)")
-		backup_docker_menu	
-		;;
+		backup_docker_menu	;;
 	
 	"8)")
 		backup_git_menu
@@ -78,6 +90,7 @@ do
 choise=$(whiptail --title "WiFi" --menu "Make your choice" 16 100 10\
 	"1)" "WiFi down" \
 	"2)" "WiFi up" \
+	"3)" "WiFi restart script" \
 	"10)" "Exit"\
 	3>&2 2>&1 1>&3)
 
@@ -88,7 +101,10 @@ echo $choise
 	;;
 	"2)")
 		wifi up
-		;;	
+		;;
+        "3)")
+                /etc/init.d/wifi restart
+                ;;	
 	"10)")
 		echo "Exit"
 		break
@@ -99,5 +115,4 @@ echo $choise
     fi
 done
 }
-
 
